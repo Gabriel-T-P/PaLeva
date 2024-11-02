155b82ad43fdd9afb957da988e1721b664b14ef6
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2024-11-02 17:54:44 -0300

    Começo da visualização do histórico de preços

--- spec/models/price_history_spec.rb
+++ spec/models/price_history_spec.rb
@@ -55,6 +55,33 @@ RSpec.describe PriceHistory, type: :model do
 
       expect(result.price).to eq portion.price
     end
+
+    it 'segundo price history é o único com current true' do
+      user = User.create!(first_name: 'Carlos', last_name: 'Jonas', cpf: CPF.generate, email: 'carlosjonas@email.com', password: '1234567891011')
+      establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", user: user, 
+                                            cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+      dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+      portion = Portion.create!(name: 'Lasanha Grande', description: 'Lasanha de carne, serve 4 pessoas', price: '52.00', item: dish)
+      portion.update(price: '50.00')
+
+      result1 = portion.price_histories.first
+      result2 = portion.price_histories.last
+
+      expect(result1.current).to be false
+      expect(result2.current).to be true
+    end
+
+    it 'so cria novo price history se for update no price da porção' do
+      user = User.create!(first_name: 'Carlos', last_name: 'Jonas', cpf: CPF.generate, email: 'carlosjonas@email.com', password: '1234567891011')
+      establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", user: user, 
+                                            cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+      dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+      portion = Portion.create!(name: 'Lasanha Grande', description: 'Lasanha de carne, serve 4 pessoas', price: '52.00', item: dish)
+      portion.update(name: 'Lasanha BIG')
+
+      result = portion.price_histories.size
+
+      expect(result).to eq 1  
+    end
   end
-  
 end
---
+++ spec/system/price_history/user_view_price_history_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe 'usuário vê histórico de preços' do
+  it 'pela página da porção de um prato' do
+    user = User.create!(first_name: 'Carlos', last_name: 'Jonas', cpf: CPF.generate, email: 'carlosjonas@email.com', password: '1234567891011')
+    establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", user: user, 
+                                          cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+    dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+    portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+
+    login_as user
+    visit root_path
+    click_on 'Meu Estabelecimento'
+    click_on 'Lasanha'
+    click_on 'Meia Lasanha'
+
+    expect(page).to have_content 'Histórico de Preços'
+    expect(page).to have_content 'Preço'
+    expect(page).to have_content 'Data de Adição'
+    expect(page).to have_content 'Data de Remoção'
+    expect(page).to have_content 'Status'
+    expect(page).to have_content '7,50'
+    expect(page).to have_content 'Atual'
+  end
+  
+  it 'pela página da porção de uma bebida' do
+    user = User.create!(first_name: 'Carlos', last_name: 'Jonas', cpf: CPF.generate, email: 'carlosjonas@email.com', password: '1234567891011')
+    establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", user: user, 
+                                          cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+    beverage = Beverage.create!(name: 'Água', description: 'Água mineral', calories: '40', item_type: 'beverage', establishment: establishment, alcoholic: false)
+    portion = Portion.create!(name: 'Água 250 ml', description: 'Água mineral em copo plástico de 250ml', price: 2.50, item: beverage)
+
+    login_as user
+    visit root_path
+    click_on 'Meu Estabelecimento'
+    click_on 'Água'
+    click_on 'Água 250 ml'
+
+    expect(page).to have_content 'Histórico de Preços'
+    expect(page).to have_content 'Preço'
+    expect(page).to have_content 'Data de Adição'
+    expect(page).to have_content 'Data de Remoção'
+    expect(page).to have_content 'Status'
+    expect(page).to have_content '2,50'
+    expect(page).to have_content 'Atual'
+  end
+  
+end
