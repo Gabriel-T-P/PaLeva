7d594938dcba69c5e75c1935bf8d6c94d12f03ac
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2024-11-02 22:04:08 -0300

    -Testes com histórico de preços terminado -I18n aplicado na seção do histórico de preços

--- spec/system/price_history/user_view_price_history_spec.rb
+++ spec/system/price_history/user_view_price_history_spec.rb
@@ -45,4 +45,26 @@ describe 'usuário vê histórico de preços' do
     expect(page).to have_content 'Atual'
   end
   
+  it 'e vê um preço antigo com current false' do
+    user = User.create!(first_name: 'Carlos', last_name: 'Jonas', cpf: CPF.generate, email: 'carlosjonas@email.com', password: '1234567891011')
+    establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", user: user, 
+                                          cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+    dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+    portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+    portion.update(price: 8.99)
+
+    login_as user
+    visit root_path
+    click_on 'Meu Estabelecimento'
+    click_on 'Lasanha'
+    click_on 'Meia Lasanha'
+
+    expect(page).to have_content 'Data de Adição'
+    expect(page).to have_content 'Data de Remoção'
+    expect(page).to have_content 'Status'
+    expect(page).to have_content 'Preço'
+    expect(page).to have_content 'Removido'
+    expect(page).to have_content '8,99'    
+  end
+  
 end
