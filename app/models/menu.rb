9aad4d678f83ca15b670bd2ca6713a53bc32751a
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2024-11-06 00:42:59 -0300

    inicio da validação do model menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,8 @@
 class Menu < ApplicationRecord
-  has_many :item_menus
+  has_many :item_menus, dependent: :destroy
   has_many :items, through: :item_menus
+
+  validates :name, presence: true
+  validates :name, uniqueness: { scope: :establishment_id }
+  
 end
