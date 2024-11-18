76aa8a5c9c8e282e636a9fa2c08af641a22e86d3
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2024-11-18 20:44:51 -0300

    -adicionado botão de remover item do carrinho na página dos pedidos -I18n adicionado para flash message de sucesso

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,9 +1,11 @@
 class CartsController < ApplicationController
-  
+  before_action :authenticate_user!
+
   def remove_item
-    portion_id = params[:portion_id]
+    portion_id = params.require(:portion_id)
     @cart.remove_item(portion_id)
-    redirect_to root_path, notice: 'Item removido com sucesso.'
+    flash[:notice] = t '.notice'
+    redirect_back(fallback_location: root_path)
   end
   
 end
--- spec/system/order/user_removes_order_item_spec.rb
+++ spec/system/order/user_removes_order_item_spec.rb
@@ -18,6 +18,24 @@ describe 'usuário remove item do pedido' do
     end  
   end
   
+  it 'pela página do pedido' do
+    establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                            cnpj: CNPJ.generate, email: 'carlosjonas@email.com', phone_number: '99999043113')
+    user = User.create!(first_name: 'Carlos', last_name: 'Jonas', cpf: CPF.generate, email: 'carlosjonas@email.com', password: '1234567891011', establishment: establishment)
+    dish = Item.create!(name: 'Pão de Queijo', description: 'Polvilho e queijo assado no forno', calories: '50', item_type: 'dish', establishment: establishment)
+    portion = Portion.create!(name: 'Pequeno', description: 'Uma unidade pequena de pão de queijo', price: 1.50, item: dish)
+
+    login_as user
+    visit establishment_item_portion_path(establishment, dish, portion)
+    fill_in 'Quantidade',	with: 1
+    click_on 'Adicionar'
+    click_on 'Ver Pedido'
+
+    within '#OrderItems' do
+      expect(page).to have_button 'x'  
+    end
+  end
+
   it 'com sucesso' do
     establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
                                             cnpj: CNPJ.generate, email: 'carlosjonas@email.com', phone_number: '99999043113')
@@ -33,9 +51,27 @@ describe 'usuário remove item do pedido' do
       click_on 'x'
     end
 
-    expect(page).to have_content 'Item removido com sucesso'  
+    expect(page).to have_content 'Item removido com sucesso'
     expect(page).not_to have_content 'Pão de Queijo'  
     expect(page).not_to have_content 'R$ 1,50x 1'
   end
   
+  it 'e volta para a página que estava antes após excluir' do
+    establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                            cnpj: CNPJ.generate, email: 'carlosjonas@email.com', phone_number: '99999043113')
+    user = User.create!(first_name: 'Carlos', last_name: 'Jonas', cpf: CPF.generate, email: 'carlosjonas@email.com', password: '1234567891011', establishment: establishment)
+    dish = Item.create!(name: 'Pão de Queijo', description: 'Polvilho e queijo assado no forno', calories: '50', item_type: 'dish', establishment: establishment)
+    portion = Portion.create!(name: 'Pequeno', description: 'Uma unidade pequena de pão de queijo', price: 1.50, item: dish)
+
+    login_as user
+    visit establishment_item_portion_path(establishment, dish, portion)
+    fill_in 'Quantidade',	with: 1
+    click_on 'Adicionar'
+    visit establishment_path(establishment)
+    within '#Cart' do
+      click_on 'x'
+    end
+
+    expect(current_path).to eq establishment_path(establishment)
+  end
 end
