fb30d6b2e2501e741b9308a14b4fbfd6566f2da5
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2024-11-19 23:56:52 -0300

    -adicionado testes para a tela de visualização de todos os cardápios

--- spec/system/menu/user_view_menus_by_sidebar_spec.rb
+++ spec/system/menu/user_view_menus_by_sidebar_spec.rb
@@ -20,4 +20,26 @@ describe 'usuário vê cardápio pela navegação na sidebar' do
     expect(page).to have_content 'Teste 3'
   end
   
+  it 'e não está logado' do
+    establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                            cnpj: CNPJ.generate, email: 'carlosjonas@email.com', phone_number: '99999043113')
+    user = User.create!(first_name: 'Carlos', last_name: 'Jonas', cpf: CPF.generate, email: 'carlosjonas@email.com', password: '1234567891011', establishment: establishment)
+
+    visit menus_path
+
+    expect(current_path).to eq new_user_session_path  
+  end
+  
+  it 'e não é admin' do
+    establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                            cnpj: CNPJ.generate, email: 'carlosjonas@email.com', phone_number: '99999043113')
+    user = Employee.create!(first_name: 'Carlos', last_name: 'Jonas', cpf: CPF.generate, email: 'carlosjonas@email.com', password: '1234567891011', establishment: establishment)
+    
+    login_as user
+    visit menus_path
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Você não possui acesso a essa página'    
+  end
+  
 end
