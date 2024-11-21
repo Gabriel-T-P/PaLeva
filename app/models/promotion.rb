ab1dad68c689d116e3db5169eb65324796ada89d
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2024-11-21 20:22:10 -0300

    -adicionado validação para presença de porções na promoção -correção dos outros testes de validação por conta das porções

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,7 +1,7 @@
 class Promotion < ApplicationRecord
   has_many :portions
 
-  validates :name, :percentage, :start_date, :end_date, presence: true
+  validates :name, :percentage, :start_date, :end_date, :portions, presence: true
   validates :percentage, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
   validates :use_limit, numericality: { greater_than_or_equal_to: 0, only_integer: true, allow_nil: true }
   validates :start_date, comparison: { less_than: :end_date }
--- spec/models/promotion_spec.rb
+++ spec/models/promotion_spec.rb
@@ -4,7 +4,11 @@ RSpec.describe Promotion, type: :model do
   describe '#valid?' do
     context 'quando Nome' do
       it 'estiver ausente' do
-        promotion = Promotion.new(name: '', percentage: 0.75, use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: '', percentage: 0.75, use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date, portions: [portion])
 
         result = promotion.valid?
 
@@ -15,7 +19,11 @@ RSpec.describe Promotion, type: :model do
     
     context 'quando Porcentagem' do
       it 'estiver ausente' do
-        promotion = Promotion.new(name: 'Teste01', percentage: '', use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: '', use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date, portions: [portion])
         
         result = promotion.valid?
 
@@ -23,7 +31,11 @@ RSpec.describe Promotion, type: :model do
       end
       
       it 'não for um número' do
-        promotion = Promotion.new(name: 'Teste01', percentage: 'asdaw', use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 'asdaw', use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date, portions: [portion])
         
         result = promotion.valid?
 
@@ -31,7 +43,11 @@ RSpec.describe Promotion, type: :model do
       end
       
       it 'for menor do que 0' do
-        promotion = Promotion.new(name: 'Teste01', percentage: -1, use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: -1, use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date, portions: [portion])
         
         result = promotion.valid?
 
@@ -39,7 +55,11 @@ RSpec.describe Promotion, type: :model do
       end
       
       it 'for maior do que 1' do
-        promotion = Promotion.new(name: 'Teste01', percentage: 2, use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 2, use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date, portions: [portion])
         
         result = promotion.valid?
 
@@ -49,7 +69,11 @@ RSpec.describe Promotion, type: :model do
     
     context 'quando Limite de Uso' do
       it 'estiver ausente' do
-        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: '', start_date: Date.current, end_date: 1.day.from_now.to_date)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: '', start_date: Date.current, end_date: 1.day.from_now.to_date, portions: [portion])
         
         result = promotion.valid?
 
@@ -57,7 +81,11 @@ RSpec.describe Promotion, type: :model do
       end
 
       it 'não for um número' do
-        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 'avad', start_date: Date.current, end_date: 1.day.from_now.to_date)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 'avad', start_date: Date.current, end_date: 1.day.from_now.to_date, portions: [portion])
         
         result = promotion.valid?
 
@@ -65,7 +93,11 @@ RSpec.describe Promotion, type: :model do
       end
       
       it 'não for um número inteiro' do
-        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 1.5, start_date: Date.current, end_date: 1.day.from_now.to_date)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 1.5, start_date: Date.current, end_date: 1.day.from_now.to_date, portions: [portion])
         
         result = promotion.valid?
 
@@ -73,7 +105,11 @@ RSpec.describe Promotion, type: :model do
       end
       
       it ' for um número menor que 0' do
-        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: -1, start_date: Date.current, end_date: 1.day.from_now.to_date)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: -1, start_date: Date.current, end_date: 1.day.from_now.to_date, portions: [portion])
         
         result = promotion.valid?
 
@@ -83,7 +119,11 @@ RSpec.describe Promotion, type: :model do
     
     context 'quando Data de Início' do
       it 'estiver ausente' do
-        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: '', end_date: 1.day.from_now.to_date)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: '', end_date: 1.day.from_now.to_date, portions: [portion])
         
         result = promotion.valid?
 
@@ -91,7 +131,11 @@ RSpec.describe Promotion, type: :model do
       end
       
       it 'não for uma data' do
-        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: 'awdwa', end_date: 1.day.from_now.to_date)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: 'awdwa', end_date: 1.day.from_now.to_date, portions: [portion])
         
         result = promotion.valid?
 
@@ -99,7 +143,11 @@ RSpec.describe Promotion, type: :model do
       end
       
       it 'for marcada para depois de Data de Fim' do
-        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: 1.week.from_now.to_date, end_date: Date.current)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: 1.week.from_now.to_date, portions: [portion], end_date: Date.current)
         
         result = promotion.valid?
 
@@ -107,18 +155,25 @@ RSpec.describe Promotion, type: :model do
       end
       
       it 'for igual a Data de Fim' do
-        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: Date.current, end_date: Date.current)
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: Date.current, end_date: Date.current, portions: [portion])
         
         result = promotion.valid?
 
         expect(result).to be false
       end
-      
     end
     
     context 'quando Data de Fim' do
       it 'estiver ausente' do
-        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: Date.current, end_date: '')
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: Date.current, end_date: '', portions: [portion])
         
         result = promotion.valid?
 
@@ -126,7 +181,30 @@ RSpec.describe Promotion, type: :model do
       end
     end
     
+    context 'quando Porções' do
+      it 'estiverem ausentes' do
+        establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                              cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+        dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+        portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date)
+        
+        result = promotion.valid?
+
+        expect(result).to be false
+      end
+    end
+    
+    it 'quando estiver tudo certo' do
+      establishment = Establishment.create!(corporate_name: 'Carlos LTDA', trade_name: "Carlo's Café", full_address: "Rio Branco, Deodoro", 
+                                            cnpj: '42.182.510/0001-77', email: 'carlosjonas@email.com', phone_number: '99999043113')
+      dish = Item.create!(name: 'Lasanha', description: 'Carne, macarrão e molho', calories: '340', item_type: 'dish', establishment: establishment)
+      portion = Portion.create!(name: 'Meia Lasanha', description: 'Lasanha de carne para 1 pessoa', price: 7.50, item: dish)
+      promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date, portions: [portion])
+      
+      result = promotion.valid?
+
+      expect(result).to be true
+    end
   end
-  
-  
 end
