b10dbb904d06d163b0ea964f0110244b8aa94f07
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2024-11-21 19:30:49 -0300

    -testes de validação para presença adicionados

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,3 +1,7 @@
 class Promotion < ApplicationRecord
   has_many :portions
+
+  validates :name, :percentage, :start_date, :end_date, presence: true
+  
+  
 end
--- spec/models/promotion_spec.rb
+++ spec/models/promotion_spec.rb
@@ -1,4 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Promotion, type: :model do
+  describe '#valid?' do
+    context 'quando Nome' do
+      it 'estiver ausente' do
+        promotion = Promotion.new(name: '', percentage: 0.75, use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date)
+
+        result = promotion.valid?
+
+        expect(result).to be false  
+      end
+      
+    end
+    
+    context 'quando Porcentagem' do
+      it 'estiver ausente' do
+        promotion = Promotion.new(name: 'Teste01', percentage: '', use_limit: 20, start_date: Date.current, end_date: 1.day.from_now.to_date)
+        
+        result = promotion.valid?
+
+        expect(result).to be false  
+      end
+      
+    end
+    
+    context 'quando Limite de Uso' do
+      it 'estiver ausente' do
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: '', start_date: Date.current, end_date: 1.day.from_now.to_date)
+        
+        result = promotion.valid?
+
+        expect(result).to be true
+      end
+    end
+    
+    context 'quando Data de Início' do
+      it 'estiver ausente' do
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: '', end_date: 1.day.from_now.to_date)
+        
+        result = promotion.valid?
+
+        expect(result).to be false
+      end
+    end
+    
+    context 'quando Data de Fim' do
+      it 'estiver ausente' do
+        promotion = Promotion.new(name: 'Teste01', percentage: 0.5, use_limit: 20, start_date: Date.current, end_date: '')
+        
+        result = promotion.valid?
+
+        expect(result).to be false
+      end
+    end
+    
+  end
+  
+  
 end
